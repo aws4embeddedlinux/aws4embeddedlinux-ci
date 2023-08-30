083e616ad59b6839f6f0b5b42cac24e29ecddeb7
alinadima <dimaalin@amazon.de>
2023-08-30 22:01:10 +0200

    Initial commit on repo move.

---
+++ docs/setup.md
@@ -0,0 +1,3 @@
+# Setting Up
+
+see an example how to use this lib in [`examples`](../../examples/docs/setup.md)
---
+++ lib/index.ts
@@ -0,0 +1,6 @@
+export * from "./network";
+export * from "./build-image-data";
+export * from "./build-image-repo";
+export * from "./build-image-pipeline";
+export * from "./demo-pipeline";
+export * from "./constructs/source-repo";
