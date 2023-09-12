adcd11e9cac823157f6f4d10b8fd64e8c8e2511f
glimsdal <glimsdal@amazon.com>
2023-09-12 15:21:25 -0500

    add -o pipefail to ami creation script

--- assets/create-ec2-ami.sh
+++ assets/create-ec2-ami.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
-set -e
+set -eo pipefail
 [ "$DEBUG" == 'true' ] && set -x
 
 ARGC=$#
